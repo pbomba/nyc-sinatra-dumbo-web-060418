78fda863e62e8eead2b7a3df101147d4ddd2a8e3
pbomba <paul@paulbomba.com>
2018-06-28 16:31:20 -0400

    got everything except edtiging figures up

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,14 +3,24 @@ class FiguresController < ApplicationController
 
   get '/figures' do
 
+  	@figures = Figure.all
     erb :'/figures/index'
   end
 
   get '/figures/new' do
     @landmarks = Landmark.all
-    # binding.pry
     erb :'/figures/new'
   end
+
+  post '/figures' do
+  	@figure = Figure.create(name:params[:name])
+  	redirect "/figures/#{@figure.id}"
+  end
+
+  get "/figures/:id" do
+	@figure = Figure.find(params[:id]) 
+	erb :'/figures/show' 
+	end
 
 
 end
